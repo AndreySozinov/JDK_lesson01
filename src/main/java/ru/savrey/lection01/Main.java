e7593366a657e4016156b1663b780fff4bd9a82a
AndreySozinov <savrey@ya.ru>
2023-10-01 20:19:19 +0500

    Seminar01 done

--- src/main/java/ru/savrey/lection01/Main.java
+++ src/main/java/ru/savrey/lection01/Main.java
@@ -2,6 +2,8 @@ package ru.savrey.lection01;
 
 public class Main {
     public static void main(String[] args) {
-        new GameWindow();
+        //new GameWindow();
+        //new ServerWindow();
+        new ClientGUI();
     }
 }
